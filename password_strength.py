dacffa07b449b193ff7071f3c047ebbe9b9ad093
yoniRos8 <yonirosis@gmail.com>
2025-07-23 18:26:34 +0300

    password_new

--- password_strength.py
+++ password_strength.py
@@ -1,31 +1,32 @@
-def password_strength(password)
+def password_strength(password):
     count_points=0#סופר את הנקודות על כל פרמטר
     if(len(password)>=8 and len(password)<=11):
-        count+=1
+        count_points+=1
     if(len(password)>=12):
-        count=+2
+        count_points=+2
     common_passwords=["password","123456","1234","admin","000000","111111","abc123","qwerty"]
     common=password.lower() in common_passwords
-    if(common):
-        count-=2
+    if(common==True):
+        count_points-=2
     has_lower = any(c.islower() for c in password)
     has_upper = any(c.isupper() for c in password)
     has_digit = any(c.isdigit() for c in password)
     has_special = any(not c.isalnum() for c in password)
     if(has_lower):
-        count+=1
+        count_points+=1
     if(has_upper):
-        count+=1
+        count_points+=1
     if(has_digit):
-        count+=1
+        count_points+=1
     if(has_special):
-        count+=1
+        count_points+=1
 
-    if(count<=0):
-        print("Weak password, Try a new one!")
-    if(count<=3 and count>=1):
-        print("Mid password")
-    if(count>3):
-        print("Strong password, Nice joob!!!") 
+    if(count_points<=0):
+        return("Weak password, Try a new one!")
+    if(count_points<=3 and count_points>=1):
+        return("Mid password")
+    if(count_points>3):
+        return("Strong password, Nice job!!!") 
 
-    
+password=input("Enter a password please: ")
+print(password_strength(password))
